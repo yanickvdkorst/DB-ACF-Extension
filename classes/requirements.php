cfe2dcc68659dd0ef070624b0becc6f3049f4f80
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2026-01-02 10:18:00 +0100

    Initial plugin

---
+++ classes/requirements.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace DB_ACF_UI;
+
+class Requirements {
+
+	use Singleton;
+
+	/**
+	 * All about requirements checks
+	 *
+	 * @return bool
+	 */
+	public function check_requirements() {
+
+		if ( ! function_exists( 'acf' ) ) {
+			$this->display_error( __( 'Advanced Custom Fields is a required plugin.', 'acf-flexible-content-extended' ) );
+			return false;
+		}
+
+		if ( $this->version_compare( '5.6.0', acf()->version, '>' ) ) {
+			$this->display_error( __( 'Advanced Custom Fields must be on version 5.6.0 or above.', 'acf-flexible-content-extended' ) );
+			return false;
+		}
+
+		return true;
+	}
+
+	// Display message and handle errors
+	public function display_error( $message ) {
+		trigger_error( $message );
+
+		add_action( 'admin_notices', function () use ( $message ) {
+			printf( '<div class="notice error is-dismissible"><p>%s</p></div>', $message );
+		} );
+
+		// Deactive self
+		add_action( 'admin_init', function () {
+			deactivate_plugins( ACF_FCE_ACF_OPTIONS_MAIN_FILE_DIR );
+			unset( $_GET['activate'] );
+		} );
+	}
+
+	// Compare versions
+	private function version_compare( $ver1, $ver2, $operator = null ) {
+    $p = '#(\.0+)+($|-)#';
+    $ver1 = preg_replace( $p, '', $ver1 );
+    $ver2 = preg_replace( $p, '', $ver2 );
+    return isset( $operator ) ? version_compare( $ver1, $ver2, $operator ) : version_compare( $ver1, $ver2 );
+	}
+}
---
+++ db-acf-ui.php
@@ -0,0 +1,159 @@
+<?php
+/*
+Plugin Name: DB ACF Extension
+Description: Aangepaste ACF interface voor Digitale Bazen
+Version: 1.1.6
+Author: Digitale Bazen
+Text Domain: db-acf-ui
+
+Icon: assets/images/icon-128x128.png
+Banner: assets/images/banner-772x250.png
+Banner-HighRes: assets/images/banner-1544x500.png
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * ---------------------------
+ * Constants
+ * ---------------------------
+ */
+define( 'DB_ACF_UI_VERSION', '1.1.6' );
+define( 'DB_ACF_UI_MIN_PHP_VERSION', '8.0' );
+
+define( 'DB_ACF_UI_FILE', __FILE__ );
+define( 'DB_ACF_UI_DIR', plugin_dir_path( __FILE__ ) );
+define( 'DB_ACF_UI_URL', plugin_dir_url( __FILE__ ) );
+define( 'DB_ACF_UI_SLUG', 'db-acf-extension' );
+
+// GitHub repo info
+define( 'DB_ACF_UI_GITHUB_REPO', 'yanickvdkorst/DB-ACF-Extension' );
+
+/**
+ * ---------------------------
+ * PHP version check
+ * ---------------------------
+ */
+if ( version_compare( PHP_VERSION, DB_ACF_UI_MIN_PHP_VERSION, '<' ) ) {
+    add_action( 'admin_notices', function () {
+        echo '<div class="notice notice-error"><p>DB ACF Extension vereist PHP 8.0 of hoger.</p></div>';
+    } );
+    return;
+}
+
+/**
+ * ---------------------------
+ * Autoload
+ * ---------------------------
+ */
+require_once DB_ACF_UI_DIR . 'autoload.php';
+
+/**
+ * ---------------------------
+ * Init plugin
+ * ---------------------------
+ */
+add_action( 'plugins_loaded', function () {
+    if ( ! function_exists( 'acf' ) ) {
+        add_action( 'admin_notices', function () {
+            echo '<div class="notice notice-error"><p>Advanced Custom Fields is vereist voor DB ACF Extension.</p></div>';
+        } );
+        return;
+    }
+
+    \DB_ACF_UI\Main::get_instance();
+} );
+
+/**
+ * ---------------------------
+ * GitHub updater
+ * ---------------------------
+ */
+
+// Inject update info
+add_filter( 'pre_set_site_transient_update_plugins', function ( $transient ) {
+
+    if ( empty( $transient->checked ) ) {
+        return $transient;
+    }
+
+    $plugin_slug = plugin_basename( DB_ACF_UI_FILE );
+
+    $response = wp_remote_get(
+        'https://api.github.com/repos/' . DB_ACF_UI_GITHUB_REPO . '/releases/latest',
+        [
+            'headers' => [ 'User-Agent' => 'WordPress' ],
+            'timeout' => 20,
+        ]
+    );
+
+    if ( is_wp_error( $response ) ) {
+        return $transient;
+    }
+
+    $data = json_decode( wp_remote_retrieve_body( $response ) );
+
+    if ( empty( $data->tag_name ) ) {
+        return $transient;
+    }
+
+    $remote_version = ltrim( $data->tag_name, 'v' );
+
+    if ( version_compare( $remote_version, DB_ACF_UI_VERSION, '>' ) ) {
+
+        $transient->response[ $plugin_slug ] = (object) [
+            'slug'        => DB_ACF_UI_SLUG,
+            'plugin'      => $plugin_slug,
+            'new_version' => $remote_version,
+            'url'         => $data->html_url,
+            'package'     => sprintf(
+                'https://github.com/%s/archive/refs/tags/%s.zip',
+                DB_ACF_UI_GITHUB_REPO,
+                $data->tag_name
+            ),
+        ];
+    }
+
+    return $transient;
+} );
+
+// Plugin info popup
+add_filter( 'plugins_api', function ( $res, $action, $args ) {
+
+    if ( $action !== 'plugin_information' || $args->slug !== DB_ACF_UI_SLUG ) {
+        return $res;
+    }
+
+    $response = wp_remote_get(
+        'https://api.github.com/repos/' . DB_ACF_UI_GITHUB_REPO . '/releases/latest',
+        [
+            'headers' => [ 'User-Agent' => 'WordPress' ],
+            'timeout' => 20,
+        ]
+    );
+
+    if ( is_wp_error( $response ) ) {
+        return $res;
+    }
+
+    $data = json_decode( wp_remote_retrieve_body( $response ) );
+
+    return (object) [
+        'name'          => 'DB ACF Extension',
+        'slug'          => DB_ACF_UI_SLUG,
+        'version'       => ltrim( $data->tag_name, 'v' ),
+        'author'        => 'Digitale Bazen',
+        'homepage'      => $data->html_url,
+        'download_link' => sprintf(
+            'https://github.com/%s/archive/refs/tags/%s.zip',
+            DB_ACF_UI_GITHUB_REPO,
+            $data->tag_name
+        ),
+        'sections'      => [
+            'description' => 'Aangepaste ACF interface voor Digitale Bazen.',
+            'changelog'   => $data->body ?: 'Geen changelog opgegeven.',
+        ],
+    ];
+}, 20, 3 );
